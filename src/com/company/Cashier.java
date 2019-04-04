322c5853adf0e4596480451ca77988f8e0a59de8
abbeyrcallow <abbeyrcallow@gmail.com>
2019-04-04 12:32:01 -0500

    putting calculations into "total"

--- src/com/company/Cashier.java
+++ src/com/company/Cashier.java
@@ -2,35 +2,34 @@ package com.company;
 import java.util.Scanner;
 
 public class Cashier extends employee{
-    double hourly = 0;
+    int hourly = 0;
     int hoursWorked = 0;
+    int total;
 
 
      void calcWeeklyPay(){
-        System.out.println("Enter employee name : ");
+
+         System.out.println("Enter employee name : ");
         Scanner scanner = new Scanner(System.in);
         setName(scanner.nextLine());
-        System.out.println("Enter employee ID ");
+        System.out.println("Enter employee ID : ");
         setEmployeeId(scanner.nextInt());
-        System.out.println("Enter hourly rate");
-        hourly = scanner.nextDouble();
-        System.out.println("Enter hours worked");
+        System.out.println("Enter hourly rate : ");
+        hourly = scanner.nextInt();
+        System.out.println("Enter hours worked this week : ");
         hoursWorked = scanner.nextInt();
 
-        System.out.println(hourly * hoursWorked) ;
+        total();
 
+        System.out.println("-Employee information-" + "\nname : " + name + "\nID number : " +
+                           employeeId + "\nHourly rate : " + hourly + "\nWeekly payroll for " + name + " : " + "$" + total);
+     }
 
-
+    @Override
+    public void total() {
+        super.total();
+        total = hourly * hoursWorked;
     }
-
-
-
-
-
-
-
-
-
 }
 
 
