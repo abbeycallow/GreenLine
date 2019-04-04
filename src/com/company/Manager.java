daa7e368c98374106453d85700b39ef49c9cc809
abbeyrcallow <abbeyrcallow@gmail.com>
2019-04-04 12:06:27 -0500

    my updates

--- src/com/company/Manager.java
+++ src/com/company/Manager.java
@@ -1,5 +1,24 @@
 package com.company;
 
-public class Manager {
+import java.util.Scanner;
 
+public class Manager extends employee {
+    int hoursWorked;
+    int salary;
+
+    void WeeklyPay(){
+
+        System.out.println("Enter employee name : ");
+        Scanner scanner = new Scanner(System.in);
+        setName(scanner.nextLine());
+        System.out.println("Enter employee ID : ");
+        setEmployeeId(scanner.nextInt());
+        System.out.println("Enter employee salary : ");
+        salary = scanner.nextInt();
+        System.out.println("Enter hours worked this week : ");
+        hoursWorked = scanner.nextInt();
+
+        System.out.println("-Employee information-" + "\nname : " + name + "\nID number : " + employeeId + "\nEmployee salary : " +
+                           salary  + "\nWeekly payroll for  " + name + " : " + "$" + (salary / hoursWorked));
+    }
 }
