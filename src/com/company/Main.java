a3551923daafed023f9cf277ad9121f37c2a95f0
abbeyrcallow <abbeyrcallow@gmail.com>
2019-04-05 09:26:03 -0500

    put menu into its own class instead of having it in main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,29 +5,13 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String user;
+        Menu menu = new Menu();
+        menu.showMenu();
 
-        Cashier cashier = new Cashier();
-        Manager manager = new Manager();
-
-        System.out.println("Welcome to GreenLine");
-        System.out.println("Enter type of employee : ");
-        Scanner scanner = new Scanner(System.in);
-        user = scanner.nextLine();
-
-        switch(user) {
-
-            case "cashier":
-                cashier.calcWeeklyPay();
-                break;
-
-
-            case "manager":
-                manager.WeeklyPay();
-                break;
-        }
     }
+
 }
+
 
 
 
